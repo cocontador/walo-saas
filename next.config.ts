bf19b6187b97edc5d0a5a4f68a95971b0096b7cf
cocontador <co.contador@duocuc.cl>
2026-04-28 13:04:46 -0400

    chore: configuracion CI para pasar prueba de github

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Esto ayuda a que el build sea más ligero y no intente conexiones raras
-  output: 'standalone', 
 };
 
 export default nextConfig;
