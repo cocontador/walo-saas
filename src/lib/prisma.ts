0ddf5804c73cf035e8e3c491d12f6777f2bb58ee
cocontador <co.contador@duocuc.cl>
2026-04-28 12:19:23 -0400

    implemente pipeline CI y corregi ruta de prisma client

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,10 @@
 import { PrismaClient } from '@/generated/prisma'
-
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['error', 'warn'],
-  })
+  new PrismaClient()
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma
-}
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
