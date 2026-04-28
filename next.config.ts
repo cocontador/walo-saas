e7023af19fb46e93188041206d9312ee07b2a7e3
cocontador <co.contador@duocuc.cl>
2026-04-28 13:16:16 -0400

    agrego export const para probar

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Configuración limpia sin "hacks"
 };
 
 export default nextConfig;
