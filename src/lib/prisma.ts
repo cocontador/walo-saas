eae889167bc537d8712a2af5cf966b17664f8ef7
cocontador <co.contador@duocuc.cl>
2026-04-28 14:13:25 -0400

    fix: confiar en el contenedor de base de datos de github actions

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,17 +4,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-// Le damos una URL de mentira segura para que el build funcione
-const safeUrl = process.env.DATABASE_URL || "postgresql://dummy:dummy@localhost:5432/dummy";
-
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: safeUrl,
-      },
-    },
-  } as any) // El "as any"  evita que TypeScript se queje de los tipos
+export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
