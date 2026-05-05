9794d693194ff40feadaeece65a6f0bf1f37233d
cocontador <co.contador@duocuc.cl>
2026-05-05 19:16:38 -0400

    se me paso el cambio de 50 a 20 caracteres antes del commit

--- src/features/auth/schema/register.schema.ts
+++ src/features/auth/schema/register.schema.ts
@@ -4,12 +4,12 @@ export const registerSchema = z.object({
     name: z
         .string()
         .min(2, 'El nombre debe tener al menos 2 caracteres.')
-        .max(50, 'El nombre no puede superar los 50 caracteres.'),
+        .max(20, 'El nombre no puede superar los 20 caracteres.'),
 
     storeName: z
         .string()
         .min(2, 'El nombre de la tienda debe tener al menos 2 caracteres.')
-        .max(50, 'El nombre de la tienda no puede superar los 50 caracteres.'),
+        .max(20, 'El nombre de la tienda no puede superar los 20 caracteres.'),
 
     email: z
         .string()
